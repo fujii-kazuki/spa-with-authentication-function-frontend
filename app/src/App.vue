12ef5eaca129320407dbce047b4a5eddf2df8c4f
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-12-26 09:43:07 +0900

    [Add] 動作確認の為のログアウトページを実装

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,5 +4,6 @@
   <div>
     <router-link to="/">Home</router-link>
     <router-link to="/login">login</router-link>
+    <router-link to="/logout">logout</router-link>
   </div>
 </template>
---
+++ app/src/views/Logout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { logout } from '@/api/auth'
+import router from '@/router'
+
+const handleLogout = () => {
+  logout().then(() => {
+    router.push('/')
+  })
+}
+</script>
+
+<template>
+  <button @click="handleLogout">Log Out</button>
+</template>
